aa53673f81b241cf458499100ac1d1ead1b75741
sanjitown <sanjitown@163.com>
2019-04-05 15:50:31 +0800

    增加参数验证

--- ssweb.py
+++ ssweb.py
@@ -20,7 +20,7 @@ render=render_jinja(
     encoding='utf-8'
 )
 
-ss=ssr.ssr()
+ss=ssr.Ssr()
 
 ## 主页
 class home():
@@ -39,26 +39,8 @@ class visitor():
 class add():
     def POST(self):
         data=web.input()
-        name= data.get('name','')
-        port=data.get('port','')
-        method=data.get('method','aes-256-cfb')
-        protocol=data.get('protocol','origin')
-        obfs=data.get('obfs','plain')
-        passwd = data.get('passwd','')
-        transfer_e = data.get('transfer_e','')
-        name = ss.next_user() if name=='' else name
-        port = ss.next_port() if port=='' else port
-        passwd = self.rand_pass() if passwd=='' else passwd
-        transfer_e = 100*1073741824 if transfer_e=='' else int(transfer_e)*1073741824
-        user={
-        'user':name,
-        'port':int(port),
-        'method':method,
-        'protocol':protocol,
-        'obfs':obfs,
-        'passwd':passwd,
-        'transfer_enable':transfer_e,
-        }
+        user = ssr.User(data).user
+        port = user.get('port')
         if ss.add(user):
             ss.del_rule(port)
             ss.add_rule(port)
@@ -67,60 +49,34 @@ class add():
         else: msg='添加失败'
         return json.dumps(msg)
 
-    def rand_pass(self):
-        import random
-        return ''.join([random.choice('''ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~-_=+(){}[]^&%$@''') for i in range(8)])
 
 ## 删除用户
 class delete():
     def POST(self):
         data = web.input()
         port = data.get("port")
-        print(port)
         msg = '删除失败'
         if port:
             port = int(port)
             user = {'port': port, }
-            print(user)
             if ss.delete(user):
-                print('ok')
-            else:print('err')
-            ss.del_rule(port)
-            ss.save_table()
-            msg = '删除成功'
+                ss.del_rule(port)
+                ss.save_table()
+                msg = '删除成功'
         return json.dumps(msg)
 
 ## 修改用户配置
 class edit():
     def POST(self):
         data = web.input()
-        name = data.get('name', '')
         port=data.get('port','')
-        passwd=data.get('passwd','')
-        method=data.get('method','')
-        protocol=data.get('protocol','')
-        obfs=data.get('obfs','')
-        transfer_e = data.get('transfer_e','')
+        user = ssr.User(data).user
         msg = '修改失败'
         if port:
             port=int(port)
-            if port<65534:
-                user={'port':port,}
+            if port<65534 and port >0:
+                user.update({'port':port})
             else:return {'code':1,'msg':'error'}
-            if name:
-                user.update({'user':name})
-            if passwd:
-                user.update({'passwd':passwd})
-            if method:
-                user.update({'method':method})
-            if protocol:
-                user.update({'protocol':protocol})
-            if obfs:
-                user.update({'obfs':obfs})
-            if transfer_e:
-                transfer_e = transfer_e.split('.')[0]
-                transfer_e = int(transfer_e)*1073741824
-                user.update({'transfer_enable':transfer_e})
             ss.edit(user)
             msg = '修改成功'
         return json.dumps(msg)
